3d250875b5eceac44a8b64c413560c398de3f881
wuzhiming <791850603@qq.com>
2020-08-21 17:52:27 +0800

    新增itemCat接口、实体类和业务实现

---
+++ taotao-manage/taotao-manage-mapper/src/main/java/top/hxq/taotao/manage/mapper/itemCat/ItemCatMapper.java
@@ -0,0 +1,13 @@
+package top.hxq.taotao.manage.mapper.itemCat;
+
+import tk.mybatis.mapper.common.Mapper;
+import top.hxq.taotao.manage.pojo.ItemCat;
+
+/**
+ * 商品类目mapper接口，继承通用Mapper
+ * @author wuzm
+ * @date 2020年8月21日 下午5:16:09
+ */
+public interface ItemCatMapper extends Mapper<ItemCat>{
+
+}
